a897ec175468fc6f716a39fca0ccdbda7dbb0928
erik <70085649+qiyjeong@users.noreply.github.com>
2022-08-26 09:50:06 -0300

    e

--- title.js
+++ title.js
@@ -16,7 +16,7 @@ function loaded() {
 
 var x = 0;
 
-var titleText = [ "w", "wx", "wx!", "wx! ", "wx! 1", "wx! 18", "wx! 188", "wx! 188 ", "wx! 188 침", "wx! 188 침묵", "wx! 188 침묵", "wx! 188 침", "wx! 188 ", "wx! 188", "wx! 18", "wx! 1", "wx! ", "wx!", "wx", "w", " " ];
+var titleText = [ "w", "wx", "wx!", "wx! ", "wx! 1", "wx! 18", "wx! 188", "wx! 188", "wx! 188", "wx! 18", "wx! 1", "wx! ", "wx!", "wx", "w", " " ];
 
 function loop() {
 
